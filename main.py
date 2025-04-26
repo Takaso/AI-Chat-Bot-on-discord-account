7ddabccf3637181fd090db35348b93d2be153ef8
Takaso Nogokui <70639611+Takaso@users.noreply.github.com>
2025-04-26 13:21:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ Now provide a reply to this message, send only your reply as a response:
 You will have the personality of a [Insert your personality prompt here], plus don't sound self repeating
 """;
 limit:int = 50; # This is the memory, the amount of messages it can recall
+typing_speed:int = 7;
 with open("config.json") as f: json_obj = json.load(f);
-token = json_obj.get("token"); API_KEY = json_obj.get("api"); prefix = json_obj.get("prefix");
+token = json_obj.get("token"); API_KEY = json_obj.get("api"); prefix = json_obj.get("prefix"); t_chan_id_ = json_obj.get("chan_id");
+typing = json_obj.get("typing");
 def send_json_request(ws, request): ws.send(json.dumps(request));
 def receive_json_response(ws):
     response = ws.recv();
@@ -49,43 +51,47 @@ def messagelogger():
         event = receive_json_response(ws);
         try:
             chan_id:str = str(event['d']['channel_id']);
-            if not chan_id in memory_channels:
-                memory_channels.update({chan_id:[]});
-            username = str(event['d']['author']['username']); message_id:str = str(event['d']['id']);
-            _input = str(event['d']['content']); xxx = str(event['d']['author']['id']);
-            sender_user_id = str(__import__("base64").b64decode(token.split(".")[0]+"==").decode("utf-8"));
-            memory_channels[chan_id].append("%s: %s" % (username if not sender_user_id==xxx else "(You)", _input if not _input.startswith(prefix) else _input[len(prefix):]));
-            if len(memory_channels[chan_id])>limit: memory_channels[chan_id] = [];
-            if (_input.startswith(prefix) or prefix=="") and not xxx==sender_user_id:
-                if not prefix=="": _input = _input[len(prefix):];
-                final_prompt:str = starting_prompt % ("".join([i+"\n" for i in memory_channels[chan_id]] if not memory_channels[chan_id]==[] else "(No messages yet)"), username, _input);
-                print(final_prompt, end="\n===========================\n");
-                requests.post(f"https://discord.com/api/v9/channels/{chan_id}/typing", headers={
-                    "Authorization": token
-                });
-                AI_response = requests.post("https://api.groq.com/openai/v1/chat/completions", headers={
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer "+API_KEY
-                }, json={
-                    "model": "llama-3.3-70b-versatile",
-                    "messages": [
-                        {
-                            "role": "user",
-                            "content": final_prompt
-                        },
-                    ],
-                });
-                AI_reply:str = AI_response.json()['choices'][0]['message']['content'];
-                # You can add a time.sleep here to make it type longer
-                requests.post(f"https://discord.com/api/v9/channels/{chan_id}/messages", headers={
-                    "Authorization": token
-                }, json={
-                    "content": AI_reply,
-                    "message_reference": {
-                        "channel_id": chan_id,
-                        "message_id": message_id
-                    }
-                });
+            if t_chan_id_ == "" or chan_id == t_chan_id_:
+                if not chan_id in memory_channels:
+                    memory_channels.update({chan_id:[]});
+                username = str(event['d']['author']['username']); message_id:str = str(event['d']['id']);
+                _input = str(event['d']['content']); xxx = str(event['d']['author']['id']);
+                sender_user_id = str(__import__("base64").b64decode(token.split(".")[0]+"==").decode("utf-8"));
+                memory_channels[chan_id].append("%s: %s" % (username if not sender_user_id==xxx else "(You)", _input if not _input.startswith(prefix) else _input[len(prefix):]));
+                if len(memory_channels[chan_id])>limit: memory_channels[chan_id] = [];
+                if (_input.startswith(prefix) or prefix=="") and not xxx==sender_user_id:
+                    if not prefix=="": _input = _input[len(prefix):];
+                    final_prompt:str = starting_prompt % ("".join([i+"\n" for i in memory_channels[chan_id]] if not memory_channels[chan_id]==[] else "(No messages yet)"), username, _input);
+                    print(final_prompt, end="\n===========================\n");
+                    requests.post(f"https://discord.com/api/v9/channels/{chan_id}/typing", headers={
+                        "Authorization": token
+                    });
+                    AI_response = requests.post("https://api.groq.com/openai/v1/chat/completions", headers={
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer "+API_KEY
+                    }, json={
+                        "model": "llama-3.3-70b-versatile",
+                        "messages": [
+                            {
+                                "role": "user",
+                                "content": final_prompt
+                            },
+                        ],
+                    });
+                    AI_reply:str = AI_response.json()['choices'][0]['message']['content'];
+                    def estimated_sleep_for_typing(text:str):
+                        return len(text)/typing_speed;
+                    if not typing == "":
+                        time.sleep(estimated_sleep_for_typing(str(AI_reply)));
+                    requests.post(f"https://discord.com/api/v9/channels/{chan_id}/messages", headers={
+                        "Authorization": token
+                    }, json={
+                        "content": AI_reply,
+                        "message_reference": {
+                            "channel_id": chan_id,
+                            "message_id": message_id
+                        }
+                    });
         except KeyError: pass;
         except Exception as y:
             print(y);
